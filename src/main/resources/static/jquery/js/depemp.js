a3d4cadc06bfbb5eb1cf4400dd6d168a59641cee
Tero Hakkarainen <Tero.Hakkarainen@solita.fi>
2017-04-01 14:10:27 +0300

    Department list populates and can be updated.

--- src/main/resources/static/jquery/js/depemp.js
+++ src/main/resources/static/jquery/js/depemp.js
@@ -1,3 +1,25 @@
-$( document ).ready(function() {
-  // Your code here.
+$(document).ready(function() {
+
+  $("#updateLink").click(function(event) {
+    updateDepartmentList();
+    event.preventDefault();
+  });
+
+  updateDepartmentList();
 });
+
+function updateDepartmentList() {
+  $.getJSON(
+	  "/departments",
+	  function(data) {
+      let tr;
+      let table = $("#departmentsTable");
+      table.find("tbody tr").remove();
+      for(let i = 0; i < data.length; i++) {
+        tr = $('<tr/>');
+        tr.append("<td>" + data[i].name + "</td>");
+        table.find("tbody").append(tr);
+      }
+    }
+  );
+}
