ab435c625bba51b2e86fd6287da54d12f18c95ba
Tero Hakkarainen <Tero.Hakkarainen@solita.fi>
2017-04-28 14:43:21 +0300

    Got TestRestTemplate test finally working.

--- src/test/java/fi/thakki/depemp/controller/DepartmentControllerTest.java
+++ src/test/java/fi/thakki/depemp/controller/DepartmentControllerTest.java
@@ -1,5 +1,7 @@
 package fi.thakki.depemp.controller;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,24 +9,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import fi.thakki.depemp.IntegrationTestConfiguration;
-import fi.thakki.depemp.model.Department;
+import fi.thakki.depemp.Application;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@ContextConfiguration(classes = IntegrationTestConfiguration.class)
+@SpringBootTest(classes = Application.class, webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource("classpath:application-integrationTest.properties")
 public class DepartmentControllerTest {
 
 	@Autowired
 	private TestRestTemplate myRestTemplate;
-
+	
 	@Test
 	public void test() {
-		ResponseEntity<Department> result = myRestTemplate.getForEntity("/departments/0", Department.class);
-		//List<?> result = myRestTemplate.getForObject("/departments", List.class);
-		System.out.println("Status: " + result.getStatusCodeValue());
+		ResponseEntity<String> result = myRestTemplate.getForEntity("/departments", String.class);
+		assertEquals(200, result.getStatusCodeValue());
+		System.out.println(result.getBody());
 	}
 }
