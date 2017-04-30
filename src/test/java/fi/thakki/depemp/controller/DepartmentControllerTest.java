50ad7bdee68b741230f3438df5b93e91f02e0151
Tero Hakkarainen <Tero.Hakkarainen@solita.fi>
2017-04-30 21:33:42 +0300

    Test for /departments service with JSON parsing.

--- src/test/java/fi/thakki/depemp/controller/DepartmentControllerTest.java
+++ src/test/java/fi/thakki/depemp/controller/DepartmentControllerTest.java
@@ -1,9 +1,14 @@
 package fi.thakki.depemp.controller;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.apache.commons.lang.RandomStringUtils;
+import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.skyscreamer.jsonassert.comparator.ArraySizeComparator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -14,6 +19,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import fi.thakki.depemp.Application;
 import fi.thakki.depemp.model.Department;
+import fi.thakki.depemp.model.LengthDomain;
 import fi.thakki.depemp.model.builder.DepartmentBuilder;
 
 @RunWith(SpringRunner.class)
@@ -25,17 +31,31 @@ public class DepartmentControllerTest {
 	private TestRestTemplate myRestTemplate;
 
 	@Autowired
-	private EntityFactory ef;
-	
+	private EntityFactory myEntityFactory;
+
 	@Test
-	public void test() {
-		String name = "foo";
-		String desc = "bar";
+	public void test() throws Exception {
+		String name = randomString(LengthDomain.SHORT_DESCRIPTION);
+		String desc = randomString(LengthDomain.LONG_DESCRIPTION);
 		Department department = new DepartmentBuilder().name(name).description(desc).get();
-		ef.persist(department);
+		myEntityFactory.persist(department);
 		
 		ResponseEntity<String> result = myRestTemplate.getForEntity("/departments", String.class);
-		assertEquals(200, result.getStatusCodeValue());
-		System.out.println(result.getBody());
+
+		assertThat(result.getStatusCode().value()).isEqualTo(200);
+		assertArraySize(1, result.getBody());
+		JSONAssert.assertEquals(
+			String.format("[{\"id\":1,\"name\":\"%s\"}]", name), result.getBody(), true);
+	}
+	
+	private static void assertArraySize(int expected, String content) throws JSONException {
+		JSONAssert.assertEquals(
+				  String.format("[%d]", expected), 
+				  content,
+				  new ArraySizeComparator(JSONCompareMode.LENIENT));		
+	}
+	
+	private static final String randomString(int length) {
+		return RandomStringUtils.random(length);
 	}
 }
