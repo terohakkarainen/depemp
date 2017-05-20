bd7d12e4f4da0bb8ba0a9dc6319fd3c84d20db97
Tero Hakkarainen <Tero.Hakkarainen@solita.fi>
2017-05-20 21:16:57 +0300

    Refactored DepartmentTransformerTest.

--- src/test/java/fi/thakki/depemp/transformer/DepartmentTransformerTest.java
+++ src/test/java/fi/thakki/depemp/transformer/DepartmentTransformerTest.java
@@ -1,40 +1,23 @@
 package fi.thakki.depemp.transformer;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import fi.thakki.depemp.Application;
-import fi.thakki.depemp.dto.DepartmentListDto;
-import fi.thakki.depemp.model.Department;
-import fi.thakki.depemp.model.EntityFactory;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class, webEnvironment = WebEnvironment.RANDOM_PORT)
-@TestPropertySource("classpath:application-integrationTest.properties")
 public class DepartmentTransformerTest {
 
-    @Autowired
-    private DepartmentTransformer myTransformerUnderTest;
+    private DepartmentTransformer myTransformerUnderTest = new DepartmentTransformer();
 
-    @Autowired
-    private EntityFactory myEntityFactory;
+    @Test(expected = NullPointerException.class)
+    public void toListDtoThrowsExceptionForNullSource() {
+        myTransformerUnderTest.toListDto(null);
+    }
 
-    @Test
-    public void toListDtoHappyCase() {
-        Department department = myEntityFactory.newDepartment("Foobar",
-                "A little bit longer description");
-        myEntityFactory.persist(department);
+    @Test(expected = NullPointerException.class)
+    public void toDetailsDtoThrowsExceptionForNullSource() {
+        myTransformerUnderTest.toDetailsDto(null);
+    }
 
-        DepartmentListDto result = myTransformerUnderTest.toListDto(department);
-
-        assertEquals(department.getId().longValue(), result.id.longValue());
-        assertEquals(department.getName(), result.name);
+    @Test(expected = NullPointerException.class)
+    public void toDepartmentThrowsExceptionForNullSource() {
+        myTransformerUnderTest.toDepartment(null);
     }
 }
